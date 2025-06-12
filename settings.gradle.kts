ebc31519ee2d09f868373ca955ba1bcb4a19fc24
Francisco Antonio Vilchez Quispe <bubaunocinconueve@gmail.com>
2025-06-11 20:21:57 -0500

    Primera Carga

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoApuntate_Apli_Movil_I"
+include(":app")
+ 
