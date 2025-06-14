bd9a582fcafc2c6740b0498c7d48e40dfd3a869a
Francisco Antonio Vilchez Quispe <bubaunocinconueve@gmail.com>
2025-06-14 12:57:37 -0500

    Creacion de Login y Registro Pantallas

--- app/src/main/java/com/example/proyectoapuntate_apli_movil_i/RegistroActivity.kt
+++ app/src/main/java/com/example/proyectoapuntate_apli_movil_i/RegistroActivity.kt
@@ -2,23 +2,23 @@ package com.example.proyectoapuntate_apli_movil_i
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val BRegistrar=findViewById<Button>(R.id.btRegistrar)
+        setContentView(R.layout.activity_registro)
 
-        BRegistrar.setOnClickListener{
-            val intent= Intent(this,RegistroActivity::class.java)
+        val BRegresar=findViewById<ImageButton>(R.id.btRegresar)
+
+        BRegresar.setOnClickListener{
+            val intent= Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
